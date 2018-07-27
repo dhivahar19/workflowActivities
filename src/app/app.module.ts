35a407d055209bd3c4ca97d3e819a4c0b98391b1
Dhivahar <dhivahar.chinnathampi@gmail.com>
2018-07-27 10:17:12 +0200

    committing task files

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import {StoreModule} from '@ngrx/store';
+import {EffectsModule} from '@ngrx/effects';
+import {SharedModule} from './core/modules/shared.module';
+import * as fromRoot from '@app-root-store';
+import {environment} from '../environments/environment';
+import {ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { MomentModule } from 'ngx-moment';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SharedModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    StoreModule.forRoot(fromRoot.reducers), /* Initialise the Central Store with Application's main reducer*/
+    EffectsModule.forRoot([]), /* Start monitoring app's side effects */
+    environment.production ? StoreDevtoolsModule.instrument({ maxAge: 50 }) : [],
+    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production }),
+    MomentModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  appApi: {
+    baseUrl: 'https://dev.toladata.io/api'
+  }
+};
